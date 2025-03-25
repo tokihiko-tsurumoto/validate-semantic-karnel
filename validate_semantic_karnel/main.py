52548fdf830514a2fe6fb95bb8263158f4c0f2c6
tokihiko-tsurumoto <tokihiko-tsurumoto@cybozu.co.jp>
2025-03-25 21:07:43 +0900

    chore:add comment

--- validate_semantic_karnel/main.py
+++ validate_semantic_karnel/main.py
@@ -13,38 +13,48 @@ from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_
     AzureChatPromptExecutionSettings,
 )
 from semantic_kernel.contents.chat_history import ChatHistory
-from semantic_kernel.utils.logging import setup_logging
 
 
 async def main():
+    dotenv.load_dotenv()
+
     # Initialize the kernel
     kernel = Kernel()
-
-    dotenv.load_dotenv()
 
     OPENAI_DEPLOYMENT_NAME = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
     OPENAI_API_KEY = os.getenv("AZURE_OPENAI_KEY")
     OPENAI_BASE_URL = os.getenv("AZURE_OPENAI_ENDPOINT")
 
-    # Add Azure OpenAI chat completion
+    # Retrieve the chat completion service for later use.
     chat_completion = AzureChatCompletion(
         deployment_name=OPENAI_DEPLOYMENT_NAME,
         api_key=OPENAI_API_KEY,
         base_url=OPENAI_BASE_URL,
     )
+
+    # Add an Azure OpneAI chat completion service to the karnel builder.
     kernel.add_service(chat_completion)
 
-    # Set the logging level for  semantic_kernel.kernel to DEBUG.
-    setup_logging()
+    # Add the logging service to the karnel to help debug the AI agent.
+    logging.basicConfig(
+        format="[%(asctime)s - %(name)s:%(lineno)d - %(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
     logging.getLogger("kernel").setLevel(logging.DEBUG)
 
-    # Add a plugin (the LightsPlugin class is defined below)
+    # With plugins, can give the AI agent the ability to run code to retrieve information from external sources or to perform actions.
+    # Add a plugin that allows the AI agent to interact with a light bulb.
     kernel.add_plugin(
         LightsPlugin(),
         plugin_name="Lights",
     )
 
-    # Enable planning
+    """
+    Semantic Kernel leverages function calling to provide planning.
+
+    To enable automatic function calling,
+    we need to create appropriate execution settings so that Semantic Kernel knows to automatically invoke the functions in the kernel when the AI agent requests them.
+    """
     execution_settings = AzureChatPromptExecutionSettings()
     execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto()
 
@@ -74,6 +84,7 @@ async def main():
 
         # Print the results
         print("Assistant > " + str(result))
+        print(LightsPlugin.lights)
 
         # Add the message from the agent to the chat history
         history.add_message(result)
